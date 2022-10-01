87655b0e2045a5a873d0f403b32a1029291260ab
robinl21 <92694179+robinl21@users.noreply.github.com>
2022-10-01 11:24:11 -0400

    template

---
+++ app.py
@@ -0,0 +1,14 @@
+from click import pass_context
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    pass
+
+#holds index htlm, base page
+
+
+# this is all the pages stuff
+
